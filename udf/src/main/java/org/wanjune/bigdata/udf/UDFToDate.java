ce19341dd05b4f639446ff413c755aad02274482
wanjune <wanjune@hotmail.com>
2019-08-09 16:35:30 +0800

    MODIFY README.md for UDFToDate

--- udf/src/main/java/org/wanjune/bigdata/udf/UDFToDate.java
+++ udf/src/main/java/org/wanjune/bigdata/udf/UDFToDate.java
@@ -3,9 +3,9 @@ package org.wanjune.bigdata.udf;
 import java.util.Date;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.hive.serde2.io.DateWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.wanjune.bigdata.util.DateUtil;
 
@@ -15,54 +15,51 @@ import org.wanjune.bigdata.util.DateUtil;
  * @author wanjune
  * @date 2019/08/07
  */
+@Description(name = "toDate", value = "_FUNC_(dateStr[, pattern]) - Returns the Date of String ",
+    extended = "Converts the specified date/time(String) to Date.\n"
+        + "Example:\n   > SELECT _FUNC_('2019/12/31') FROM src LIMIT 1;\n  2019-12-31\n"
+        + "Example:\n   > SELECT _FUNC_('2019年06月30日', 'yyyy年MM月dd日') FROM src LIMIT 1;\n  2019-06-30\n")
 public class UDFToDate extends UDF {
     private final DateWritable result = new DateWritable();
 
     public UDFToDate() {}
 
-    public DateWritable evaluate(NullWritable dateText) {
-        return null;
-    }
-
-    public DateWritable evaluate(NullWritable dateText, Text datePatternText) {
-        return null;
-    }
-
     public DateWritable evaluate(Text dateText) {
 
-        String dateString = dateText.toString();
-
-        if (StringUtils.isEmpty(dateString)) {
+        if (null == dateText || StringUtils.isEmpty(dateText.toString())) {
             return null;
         }
 
-        Date rdate = DateUtil.toDate(dateString);
+        Date rdate = DateUtil.toDate(dateText.toString());
 
-        if (null == rdate) {
+        if (null != rdate) {
+            this.result.set(new java.sql.Date(rdate.getTime()));
+            return this.result;
+        } else {
             return null;
         }
 
-        this.result.set(new java.sql.Date(rdate.getTime()));
-        return this.result;
     }
 
     public DateWritable evaluate(Text dateText, Text datePatternText) {
 
-        String dateString = dateText.toString();
-        String datePatternString = datePatternText.toString();
-
-        if (StringUtils.isEmpty(dateString)) {
+        if (null == dateText || StringUtils.isEmpty(dateText.toString())) {
             return null;
         }
 
-        Date rdate = DateUtil.toDate(dateString, datePatternString);
-
-        if (null == rdate) {
-            return null;
+        Date rdate = null;
+        if (null != datePatternText && !StringUtils.isEmpty(datePatternText.toString())) {
+            rdate = DateUtil.toDate(dateText.toString(), datePatternText.toString());
+        } else {
+            rdate = DateUtil.toDate(dateText.toString());
         }
 
-        this.result.set(new java.sql.Date(rdate.getTime()));
-        return this.result;
+        if (null != rdate) {
+            this.result.set(new java.sql.Date(rdate.getTime()));
+            return this.result;
+        } else {
+            return null;
+        }
     }
 
 }
